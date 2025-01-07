f50d5dfb6062f825c09e26313a9d2c541d8a6488
rnagripino <rnagripino@gmail.com>
2025-01-07 13:54:31 -0800

    add Services component and integrate it into the Home page

---
+++ app/components/Services.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Services = () => {
+  return (
+    <div id="services" className="w-full px-[12%] py-10 scroll-mt-20">
+      <h4 className="text-center mb-2 text-lg font-Ovo">What I offer</h4>
+      <h2 className="text-center text-5xl font-Ovo">My Services</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit,
+        molestias suscipit! Placeat, error ut? Inventore, eaque dicta vitae
+        ipsam amet rem ex tempora ratione in maxime vel reiciendis quidem ipsa.
+      </p>
+    </div>
+  );
+};
+
+export default Services;
