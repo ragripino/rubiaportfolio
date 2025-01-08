facac76459ae105703b984282bfde5e9d88a1d32
rnagripino <rnagripino@gmail.com>
2025-01-07 21:21:42 -0800

    add Services section with dynamic service cards and icons; include new graphic assets

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -1,3 +1,5 @@
+import { serviceData } from '@/assets/assets';
+import Image from 'next/image';
 import React from 'react';
 
 const Services = () => {
@@ -5,11 +7,21 @@ const Services = () => {
     <div id="services" className="w-full px-[12%] py-10 scroll-mt-20">
       <h4 className="text-center mb-2 text-lg font-Ovo">What I offer</h4>
       <h2 className="text-center text-5xl font-Ovo">My Services</h2>
-      <p>
+
+      <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit,
-        molestias suscipit! Placeat, error ut? Inventore, eaque dicta vitae
-        ipsam amet rem ex tempora ratione in maxime vel reiciendis quidem ipsa.
+        molestias suscipit! Placeat, error ut. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Quam, doloremque.
       </p>
+      <div className="grid grid-cols-4 gap-6 my-10">
+        {serviceData.map(({ icon, title, description, link }, index) => (
+          <div key={index}>
+            <Image src={icon} alt="" className="w-10" />
+            <h3 className="text-lg my-4 text-gray-700">{title}</h3>
+            <p className="text-sm text-gray-600 leading-5"></p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
