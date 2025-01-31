1f478c1924ec48f9e68ddb708c8a4a5ecac96763
rnagripino <rnagripino@gmail.com>
2025-01-30 16:04:57 -0800

    refactor Work component; update layout and integrate into Home page

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const work = () => {
+const Work = () => {
   return (
-    <div>
-      <h4 className="text-center mb-2 text-lg font-Ovo">What I offer</h4>
-      <h2 className="text-center text-5xl font-Ovo">My Services</h2>
+    <div id="work" className="w-full px-[12%] py-10 scroll-mt-20">
+      <h4 className="text-center mb-2 text-lg font-Ovo">My Portifolio</h4>
+      <h2 className="text-center text-5xl font-Ovo">My latasted Work</h2>
 
       <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit,
@@ -15,4 +15,4 @@ const work = () => {
   );
 };
 
-export default work;
+export default Work;
